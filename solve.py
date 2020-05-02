b2baf162026fcac55005c55ebf81330d0e1d8c10
a beyblade <64674352+a-beyblade@users.noreply.github.com>
2020-05-02 17:55:32 -0600

    Refactor

--- solve.py
+++ solve.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from enum import Enum
+from functools import reduce
 from typing import NamedTuple, Tuple
 
 class Weapon(NamedTuple):
@@ -7,138 +11,163 @@ class Weapon(NamedTuple):
     mag_size: int
     fire_delay: int = 0
 
+class ShotAction(Enum):
+    FULL_DELAY = 0
+    FREE_SWITCH = 1
+    INITIAL_IMMEDIATE = 2
+    INITIAL_FS = 3
+
+class Shot(NamedTuple):
+    action: ShotAction
+    weap_index: int
+    delay: int
+    next_free_switch_state: int
+
+ShotChain = Tuple[Shot, ...]
+ShotChains = Tuple[ShotChain, ...]
+
+ShotCount = Tuple[int, ...]
+
+class SolverState(NamedTuple):
+    shots: ShotCount
+    deploy_group: int
+    free_switch_state: int
+
+class SolverSolution(NamedTuple):
+    objective: int = 0
+    count: int = 1
+    chains: ShotChain = ((),)
+
+    def merge(self, other: SolverSolution) -> SolverSolution:
+        return SolverSolution(self.objective, self.count + other.count, self.chains + other.chains)
+
 class Solver(NamedTuple):
-    weapons: Tuple[Weapon]
+    weapons: Tuple[Weapon, ...]
     require_shot_at_zero = False
     free_switch_timer = 1000
     free_switch_delay = 250
     max_slow = 0
+
     memo: dict
 
     @classmethod
     def new(cls, *args, **kwargs):
         return cls(memo={}, *args, **kwargs)
 
-    def minimize_time_first_to_last(self):
+    def minimize_time_first_to_last(self) -> SolverSolution:
         s = self._get_shots_initial()
         if self.require_shot_at_zero:
-            return self._solve2(s, 0, 0, False)
+            return self._solve_initial(s, 0, 0, False)
         else:
-            return self._solve2(s, 1, self.free_switch_timer - 1, True)
+            return self._solve_initial(s, 1, self.free_switch_timer - 1, True)
 
-    def minimize_max_time_between_shots(self):
+    def minimize_max_time_between_shots(self) -> SolverSolution:
         raise NotImplementedError('this problem is hard')
 
-    def _get_shots_initial(self):
+    def _get_shots_initial(self) -> ShotCount:
         return tuple(w.mag_size for w in self.weapons)
 
-    def _solve(self, fs_timer, deploy_group, s):
-        if not any(s):
-            return 0, 1, ((),)
+    @staticmethod
+    def _merge_solution_min(a: SolverSolution, b: SolverSolution) -> SolverSolution:
+        if a.objective < b.objective:
+            return a
+        if b.objective < a.objective:
+            return b
+        return a.merge(b)
 
-        key = (fs_timer, deploy_group) + s
-        if key not in self.memo:
-            best = best_count = best_chains = None
+    def _solve(self, state: SolverState) -> SolverSolution:
+        if not any(state.shots):
+            return SolverSolution()
+
+        if state not in self.memo:
+            def next_state_generator():
+                for i, weap in enumerate(self.weapons):
+                    if state.shots[i]:
+                        next_s = Solver.remove_one_shot(state.shots, i)
+
+                        for delay, next_fs, action in (
+                            # wait for next free switch
+                            (
+                                (state.free_switch_state if state.free_switch_state or weap.deploy_group != state.deploy_group else self.free_switch_timer)
+                                    + self.free_switch_delay,
+                                self.free_switch_timer - self.free_switch_delay,
+                                ShotAction.FREE_SWITCH
+                            ),
+                            # use full switch delay
+                            (
+                                weap.switch_delay,
+                                max(0, (state.free_switch_state or self.free_switch_timer) - weap.switch_delay),
+                                ShotAction.FULL_DELAY
+                            ),
+                        ):
+                            r, count, chains = self._solve(SolverState(next_s, weap.deploy_group, next_fs))
+                            yield SolverSolution(r + delay, count, tuple((Shot(action, i, delay, next_fs),) + x for x in chains))
+            self.memo[state] = reduce(Solver._merge_solution_min, next_state_generator())
+
+        return self.memo[state]
+
+    def _solve_initial(self, shots: ShotCount, initial_fs: int, initial_delay: int, initial_action: ShotAction) -> SolverSolution:
+        if not any(shots):
+            return SolverSolution()
+
+        def next_state_generator():
             for i, weap in enumerate(self.weapons):
-                if not s[i]:
-                    continue
+                if shots[i]:
+                    r, count, chains = self._solve(SolverState(Solver.remove_one_shot(shots, i), weap.deploy_group, initial_fs))
+                    yield SolverSolution(r, count, tuple((Shot(initial_action, i, initial_delay, initial_fs),) + x for x in chains))
 
-                next_s = Solver.remove_one_shot(s, i)
-
-                for delay, next_fs, was_fs in (
-                    # wait for next free switch
-                    (
-                        (self.free_switch_timer if fs_timer == 0 and weap.deploy_group == deploy_group else fs_timer)
-                            + self.free_switch_delay,
-                        self.free_switch_timer - self.free_switch_delay,
-                        True
-                    ),
-                    # use full switch delay
-                    (
-                        weap.switch_delay,
-                        max(0, (fs_timer or self.free_switch_timer) - weap.switch_delay),
-                        False
-                    ),
-                ):
-                    r, count, chains = self._solve(next_fs, weap.deploy_group, next_s)
-                    r += delay
-                    if best is None or best > r:
-                        best, best_count, best_chains = r, count, tuple(((i, delay, next_fs, was_fs),) + x for x in chains)
-                    elif best == r:
-                        best_count += count
-                        best_chains += tuple(((i, delay, next_fs, was_fs),) + x for x in chains)
-            self.memo[key] = best, best_count, best_chains
-
-        return self.memo[key]
-
-    def _solve2(self, s, initial_fs, initial_delay, initial_was_fs):
-        best = best_count = best_chains = None
-        for i, weap in enumerate(self.weapons):
-            if not s[i]:
-                continue
-
-            r, count, chains = self._solve(initial_fs, weap.deploy_group, Solver.remove_one_shot(s, i))
-            if best is None or best > r:
-                best, best_count, best_chains = r, count, tuple(((i, initial_delay, initial_fs, initial_was_fs),) + x for x in chains)
-            elif best == r:
-                best_count += count
-                best_chains += tuple(((i, initial_delay, initial_fs, initial_was_fs),) + x for x in chains)
-
-        if best is None:
-            return 0, 1, ((),)
-
-        return best, best_count, best_chains
+        return reduce(Solver._merge_solution_min, next_state_generator())
 
     @staticmethod
-    def remove_one_shot(s, i):
+    def remove_one_shot(s: ShotCount, i: int) -> ShotCount:
         return s[:i] + (s[i] - 1,) + s[i + 1:]
 
 class Formatter(NamedTuple):
     solver: Solver
 
-    def format(self, chain) -> str:
+    def format(self, chain: ShotChain) -> str:
         raise NotImplementedError
 
 class TimingFormatter(Formatter):
-    def format(self, chain):
-        entries = []
-        not_first = False
-        for v in chain:
-            if v[3] and not_first:
-                if v[1] != self.solver.free_switch_delay:
-                    entries.append(str(v[1] - self.solver.free_switch_delay))
-                entries.append('fs {}'.format(self.solver.free_switch_delay))
-            elif v[1] or not_first:
-                entries.append(str(v[1]))
-            entries.append(self.solver.weapons[v[0]].name)
-            not_first = True
-        return ' '.join(entries)
+    def format(self, chain: ShotChain) -> str:
+        def generate_tokens():
+            free_switch_token = 'fs {}'.format(self.solver.free_switch_delay)
+
+            for shot in chain:
+                if shot.action == ShotAction.FREE_SWITCH:
+                    if shot.delay != self.solver.free_switch_delay:
+                        yield str(shot.delay - self.solver.free_switch_delay)
+                    yield free_switch_token
+                elif shot.action != ShotAction.INITIAL_IMMEDIATE:
+                    yield str(shot.delay)
+                yield self.solver.weapons[shot.weap_index].name
+
+        return ' '.join(generate_tokens())
 
 class InstructionFormatter(Formatter):
-    def format(self, chain):
-        t = 0
-        last_weap = None
-        not_first = False
-        tokens = []
-        for shot in chain:
-            t += shot[1]
-            if shot[3]:
-                if shot[1] != self.solver.free_switch_delay and not_first:
-                    tokens.append('c')
-                    tokens.append(str(t - self.solver.free_switch_delay) + ':')
-                tokens.append(chr(ord('A') + shot[0]))
-                tokens.append(str(t) + ':')
-                tokens.append('!')
-            else:
-                if last_weap == shot[0]:
-                    tokens.append('d')
-                tokens.append(chr(ord('a') + shot[0]))
+    def format(self, chain: ShotChain) -> str:
+        def generate_tokens():
+            t = 0
+            last_weap = -1
+            for shot in chain:
+                t += shot.delay
+
+                if shot.action == ShotAction.INITIAL_FS or shot.action == ShotAction.FREE_SWITCH:
+                    if shot.delay != self.solver.free_switch_delay and shot.action == ShotAction.FREE_SWITCH:
+                        yield 'c'
+                        yield str(t - self.solver.free_switch_delay) + ':'
+                    yield (chr(ord('A') + shot.weap_index))
+                else:
+                    if last_weap == shot.weap_index:
+                        yield 'd'
+                    yield chr(ord('a') + shot.weap_index)
+
                 if t:
-                    tokens.append(str(t) + ':')
-                tokens.append('!')
-            last_weap = shot[0]
-            not_first = True
-        return ' '.join(tokens)
+                    yield str(t) + ':'
+
+                yield '!'
+                last_weap = shot.weap_index
+        return ' '.join(generate_tokens())
 
 def main():
     solver = Solver.new(weapons=(
